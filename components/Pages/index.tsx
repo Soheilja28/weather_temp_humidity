c65a6e3146eecb4f800b622b2d5a17e6e7f3d609
Soheilja28 <$sohrabjalalitr28@gmail.com>
2024-09-06 19:17:26 +0330

    Update

--- components/Pages/index.tsx
+++ components/Pages/index.tsx
@@ -22,28 +22,44 @@ const Page: PageEl = (props, state, refresh, getProps) => {
       <br-x />
       <Window title={name} style={{
         minHeight: 400, margin: 10, width: "calc(100% - 20px)",
-        backgroundImage: "url('https://irmapserver.ir/research/12/sky-wallpaper-for-video-conferencing_-crc8de88257-_size1.48mb.webp')",
+        backgroundImage: "url('https://cdn.ituring.ir/research/12/sky-wallpaper-for-video-conferencing_-crc8de88257-_size1.48mb.webp')",
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         objectFit: "contain",
       }}>
 
         <c-x>
-          <br-x/>
+
+          <br-x />
+
           <f-cc>
-            <f-x style={{fontSize:30 , fontFamily:"cursive", backgroundColor:"lightblue" , borderRadius:"10px", paddingRight:5 , paddingLeft:5}}>weather</f-x>
+            <f-x style={{
+              fontSize: 30,
+              fontFamily: "cursive",
+              backgroundColor: "lightblue",
+              borderRadius: "10px",
+              paddingRight: 5,
+              paddingLeft: 5
+            }}>weather</f-x>
           </f-cc>
 
           <br-x />
           <br-x />
 
-
           <f-cc>
 
-            <f-cse style={{ width: "200px", height: "200px", backgroundColor: "skyblue", borderRadius: "20px", fontSize: "20px", boxShadow: "0 0 15px rgb(46 50 54)", opacity:"0.8"}}>
+            <f-cse style={{
+              width: "200px",
+              height: "200px",
+              backgroundColor: "skyblue",
+              borderRadius: "20px",
+              fontSize: "20px",
+              boxShadow: "0 0 15px rgb(46 50 54)",
+              opacity: "0.8"
+            }}>
 
               <f-cc>
-                <img style={{ width: 100, height: 100 }} src='https://irmapserver.ir/research/12/thermometer.png' />
+                <img style={{ width: 100, height: 100 }} src='https://cdn.ituring.ir/research/12/thermometer.png' />
 
                 <sp-1 />
 
@@ -56,25 +72,33 @@ const Page: PageEl = (props, state, refresh, getProps) => {
             <sp-1 />
 
 
-            <f-cse style={{ width: "200px", height: "200px", backgroundColor: "skyblue", borderRadius: "20px", fontSize: "20px" , boxShadow: "0 0 15px rgb(46 50 54)" , opacity:"0.8"}}>
-              
+            <f-cse style={{
+              width: "200px",
+              height: "200px",
+              backgroundColor: "skyblue",
+              borderRadius: "20px",
+              fontSize: "20px",
+              boxShadow: "0 0 15px rgb(46 50 54)",
+              opacity: "0.8"
+            }}>
+
               <f-cc>
-                <img style={{ width: 100, height: 100 }} src='https://irmapserver.ir/research/12/drop.png' />
+                <img style={{ width: 100, height: 100 }} src='https://cdn.ituring.ir/research/12/drop.png' />
 
                 <sp-1 />
 
-                <f-cc style={{fontSize:30}}>{(props.data.humidity)} %</f-cc>
+                <f-cc style={{ fontSize: 30 }}>{(props.data.humidity)} %</f-cc>
               </f-cc>
 
             </f-cse>
 
           </f-cc>
 
-          <br-x/>
-          <br-x/>
-          <br-x/>
+          <br-x />
+          <br-x />
+          <br-x />
 
-          <f-cc style={{fontSize:20}}>
+          <f-cc style={{ fontSize: 20 }}>
             tuning team
           </f-cc>
         </c-x>
@@ -94,7 +118,7 @@ export async function getServerSideProps(context) {
     usedquota, quota, quotaunit, status, regdate, expid,
     role, path, devmod, userip, } = session;
 
-  let json = await (await fetch("https://irmapserver.ir/research/api/weather/")).json()
+  let json = await (await fetch("https://cdn.ituring.ir/research/api/weather/")).json()
   let data = json.current_condition[0]
 
 
